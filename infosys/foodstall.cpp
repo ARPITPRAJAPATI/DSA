6f5705087a8eae8491e5f70e84894af953512abd
ARPITPRAJAPATI <arpitprajapati2005@gmail.com>
2026-07-10 15:29:50 +0530

    Add implementation for maximum total profit calculation in food stall problem

---
+++ infosys/foodstall.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+long long maximumTP(int foods,long long maxFoods,vector<long long>& v,vector<long long>& d
+) {
+   priority_queue<pair<long long, int>> pq;
+    for(int i=0;i<foods;i++){
+        pq.push({v[i],i});
+    }
+    long long ans = 0;
+    for(long long meal = 0;meal<maxFoods;meal++){
+        pair<long long , int > current = pq.top();
+        pq.pop();
+
+        long long currentFood = current.first;
+        if (currentFood <= 0) {
+            break;
+        }
+        int currentfoodIndex = current.second;
+        ans = ans + current.first;
+        currentFood = currentFood - d[currentfoodIndex];
+        pq.push({currentFood,currentfoodIndex});
+    }
+    return ans;
+}
+
+int main() {
+    int foods;
+    cin >> foods;
+
+    long long maxFoods;
+    cin >> maxFoods;
+
+    vector<long long> v(foods);
+    for (int i = 0; i < foods; i++) {
+        cin >> v[i];
+    }
+
+    vector<long long> d(foods);
+    for (int i = 0; i < foods; i++) {
+        cin >> d[i];
+    }
+
+    long long answer = maximumTP(foods, maxFoods, v, d);
+
+    cout << answer;
+    return 0;
+}
--- top 50/combinationSum.cpp
+++ top 50/combinationSum.cpp
@@ -13,14 +13,14 @@ public:
     void helper(int i,int target,vector<int> temp,vector<int>& candidates, vector<vector<int>>& result) {
         if (i >= candidates.size()) {
             if (target == 0) {
-                result.push_back(temp);
+                result.push_back(temp); //base condition ofcourse
             }
             return;
         }
         if (candidates[i] <= target) {
-            temp.push_back(candidates[i]);
-            helper(i, target - candidates[i], temp, candidates, result);
-            temp.pop_back();
+            temp.push_back(candidates[i]); 
+            helper(i, target - candidates[i], temp, candidates, result); // to take and explore every possible way.
+            temp.pop_back(); // forward to not take
         }
         helper(i + 1, target, temp, candidates, result);
     }
@@ -33,3 +33,9 @@ public:
     }
 };
 
+tips.. 1. make a tree for every possible value
+       2. base condition when we reach the end of the tree
+       3. take only if value is less then target.
+       4. take and not take condition
+       5. first take that element explore then remove. so => we can not take
+       6. in not take just increase i

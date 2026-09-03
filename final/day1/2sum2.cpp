18f08205c5d9f51ec3decdbeb400f9cc57b7aea3
ARPITPRAJAPATI <arpitprajapati2005@gmail.com>
2026-09-04 02:06:47 +0530

    Add implementation for Two Sum problem

---
+++ final/day1/2sum2.cpp
@@ -0,0 +1,23 @@
+lc 167
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        unordered_map<int,int>mp;
+        int sum=0;
+        
+        for(int i=0;i<numbers.size();i++){
+            sum = target-numbers[i];
+            if(mp.find(sum)!=mp.end()){
+                 if(i>mp[sum]){
+                    return {mp[sum]+1,i+1};
+                 }
+                 else{
+                    return {i+1,mp[sum]+1};
+                 }
+            }
+            mp[numbers[i]]=i;
+        }
+        return {};
+    }
+};
